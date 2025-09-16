1e1f21aeeaa6ba6f2895c3e2f82c7ab86df5c133
sheilaapr <230605110005@student.uin-malang.ac.id>
2025-09-17 01:50:57 +0700

    halo

--- lib/main.dart
+++ lib/main.dart
@@ -13,10 +13,37 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Expanded'),
         ),
-        body: Center(
-          child: Text('Hello, Flutter!'),
+        // body: Center(
+        //   child: Text('Hello, Flutter!'),
+        body: Column(
+          children: [
+            _kotakUjii(Colors.amber, 150, 'Normal'),
+            // _kotakUjii(Colors.green, 100, 'Flexible'),
+            // _kotakUjii(Colors.blue, 250, 'Expanded'),
+
+            Flexible(
+              // fit: FlexFit.tight,
+              fit: FlexFit.loose,
+              flex: 1,
+              child: _kotakUjii(Colors.green, 100, 'Flexible')),
+            Expanded(flex: 2, child: _kotakUjii(Colors.blue, 250, 'Expanded')  
+            )
+          ],
         ),
-      ),
+        ),
     );
   }
+}
+
+Container _kotakUjii(Color warna, double tinggi, String teks) {
+  return Container(
+    width: double.infinity,
+    height: tinggi,
+    color: warna,
+    alignment: const Alignment(0, 0),
+    child: Text(
+      teks,
+      style: const TextStyle(color: Colors.white, fontSize: 30),
+    ),
+  );
 }
